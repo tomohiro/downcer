651d6aadd0d5a9790ec399e6433cb0c1a2b986b4
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2012-09-10 19:25:07 +0900

    rescue

--- lib/downcer.rb
+++ lib/downcer.rb
@@ -9,8 +9,12 @@ module Downcer
     end
 
     def initialize(url)
-      @url = url
+      @url = URI.parse(url)
       @config = YAML.load_file("#{ENV['HOME']}/.downcerrc")
+    rescue URI::InvalidURIError => e
+      abort 'Usage: downcer <url>'
+    rescue SystemCallError => e
+      abort e.to_s
     end
 
     def download
@@ -20,8 +24,7 @@ module Downcer
 
     private
       def get_command
-        host = URI.parse(@url).host
-        site = @config[host] || @config['default']
+        site = @config[@url.host] || @config['default']
         return ['wget', ''] if site.nil?
 
         [site['command'], site['option']]
