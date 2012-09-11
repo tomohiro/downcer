f0555201fd3326c54b62a4cf3736846a22a59e52
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2012-09-11 18:19:34 +0900

    move cli class

--- lib/downcer.rb
+++ lib/downcer.rb
@@ -1,33 +1,4 @@
-require 'downcer/version'
-require 'yaml'
-require 'uri'
-
 module Downcer
-  class CLI
-    def self.start(*args)
-      self.new(args.first).download
-    end
-
-    def initialize(url)
-      @url = URI.parse(url)
-      @config = YAML.load_file("#{ENV['HOME']}/.downcerrc")
-    rescue URI::InvalidURIError => e
-      abort 'Usage: downcer <url>'
-    rescue SystemCallError => e
-      abort e.to_s
-    end
-
-    def download
-      command, option = get_command
-      system("#{command} #{option} '#{@url}'")
-    end
-
-    private
-      def get_command
-        site = @config[@url.host] || @config['default']
-        return ['wget', ''] if site.nil?
-
-        [site['command'], site['option']]
-      end
-  end
+  autoload :Version, 'downcer/version'
+  autoload :CLI,     'downcer/cli'
 end
